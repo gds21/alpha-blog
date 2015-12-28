7b8212ebf44adea587bb3de34b5b29e938ee6dbf
Glee <iprevent@me.com>
2015-12-28 02:03:13 +0000

    User validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-    
+    validates :username presence: true,uniqueness: {case_sensitive: false},length: {minimum:3,maxium:25}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email presence: true,uniqueness: {case_sensitive: false},format:{with: VALID_EMAIL_REGEX},
+    length: {maxium:105}
 end
